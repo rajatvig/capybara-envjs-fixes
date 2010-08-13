b89eccaa8cb90abae50b49c49ead01a4b95bf5c4
Mathijs Sterk <mathijs.sterk@itrp.com>
2010-08-13 14:30:51 +0200

    Make sure JQUERY_FADE is loaded after jQuery. Newlines in <head> tag are not accepted.

--- lib/capybara_envjs_fixes/cucumber_fixes/javascript.rb
+++ lib/capybara_envjs_fixes/cucumber_fixes/javascript.rb
@@ -45,25 +45,23 @@ module CucumberJavascript
     var valueSetter = extension.__lookupSetter__('value');
     HTMLTextAreaElement.prototype.__defineSetter__('value', valueSetter);
   }
-  MOCK_JAVASCRIPT = (MOCK_SET_TIMEOUT + MOCK_DEBUG + MOCK_ENVJS).freeze
+  MOCK_JAVASCRIPT = (MOCK_DEBUG + MOCK_ENVJS).gsub(/\n/, ' ').freeze # MOCK_SET_TIMEOUT
   MOCK_JQUERY = MOCK_JQUERY_FADE.freeze
 end
 
-Before('@javascript') do
+# Before('@javascript') do
+Before do
   @__custom_javascript = []
   Capybara.current_session.driver.rack_mock_session.after_request do
     new_body = Capybara.current_session.driver.response.body
     new_body.gsub!("<head>",
-                   %{"<head>
-                     <script type="text/javascript">
-                       #{CucumberJavascript::MOCK_JAVASCRIPT}
-                     </script>})
-    new_body.sub!("</body>",
-                  %{<script type="text/javascript">
+                   "<head><script type='text/javascript'>#{CucumberJavascript::MOCK_JAVASCRIPT}</script>");
+    new_body.sub!("</html>",
+                  %{<script type='text/javascript'>
                     #{CucumberJavascript::MOCK_JQUERY}
                     #{@__custom_javascript.join}
                     </script>
-                    </body>})
+                    </html>})
     new_body.gsub!(%r{<script src="http://[^"]+" type="text/javascript"></script>}, '')
     Capybara.current_session.driver.response.instance_variable_set('@body', new_body)
   end
